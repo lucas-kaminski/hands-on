095e1db9429eaca290df1b464f61707fa52cf245
lucas-kaminski <72414846+lucas-kaminski@users.noreply.github.com>
2021-03-30 19:13:53 -0300

    initial push curso BasicoAvancado

---
+++ BasicoAoAvancado/4 - tipos de dados/tipo_boolean.py
@@ -0,0 +1,36 @@
+'''
+Algebra booleana de George Boole
+
+True
+False
+
+(letras maiusculas)
+'''
+
+ativo = False
+logado = False
+
+'''
+Operações básicas
+'''
+#Negação (not)
+not ativo
+
+#Ou (or)
+ativo or logado
+
+#E (and)
+ativo and logado
+
+'''
+Comparações
+'''
+
+5 > 6
+5 < 6
+6 == 6 #igual
+6 <= 6 #menor ou igual
+6 >= 6 #maior ou igual
+
+
+dir(ativo)
---
+++ BasicoAoAvancado/5 - condicionais e logicos/estruturas_logicas.py
@@ -0,0 +1,18 @@
+'''
+BINÁRIOS
+    e(and)
+    ou(or)
+    é(is)
+UNITÁRIOS
+    not(não)
+
+and e or seguem as tabelas verdades
+not é negacao do boolean
+or é comparação entre dois
+is é comparação de algo com algo
+'''
+ativo = True
+logado = True
+
+print(not ativo)
+print(ativo is int)
