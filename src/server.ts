5ace14babc75ea8a01ce85528948b87e5386bdbf
Lucas Kaminski <lucas.kssilveira@gmail.com>
2021-09-11 01:43:20 -0300

    start on schema prisma

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'express-async-errors'
 import express, { json } from 'express'
 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const app = express()
 
 app
@@ -8,6 +11,9 @@ app
   .use((req, _, next) => {
     console.log(' + ', req.path, '[', req.method, ']')
     next()
+  })
+  .get('/', async (_, res) => {
+    return res.json('Server da âmago está no ar!')
   })
   .listen(3333, () => {
     console.log(`✔ server is running on port 3333`)
