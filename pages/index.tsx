f3bc79b538f1809dcc2daf7a42054127f715b2fa
lucas-kaminski <lucas.kssilveira@gmail.com>
2021-10-18 17:19:28 -0300

    started new project typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Amago Plantas - Administrativo</title>
+      <meta name="description" content="Site administrativo da loja Amago Plantas" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main> aa </main>
+
+    <footer> bb </footer>
+  </div>
+)
+
+export default Home
