2bee323da445a2c77c33c1bb5a4a83a330b09cce
Lucas Kaminski <lucas.kssilveira@gmail.com>
2022-01-18 13:48:49 -0300

    start on soap studies

---
+++ hands-on-soap/server.js
@@ -0,0 +1,19 @@
+const soap = require('soap')
+const url = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl'
+
+soap.createClient(url, (err, client) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('ok')
+    client.consultaCEP({
+      cep: '80630130'
+    }, (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(result)
+      }
+    })
+  }
+})
