12c6676b2e76a0cae938db8f4d549384e63ab0ef
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-02-19 13:06:08 -0300

    fix missing import at script createTable

--- scripts/database/createTables.py
+++ scripts/database/createTables.py
@@ -1,14 +1,13 @@
 from database.connector import Connector
+import os
 
 connection = Connector().connection
 
 for table in os.listdir("database/tables"):
-  if not table.endswith(".sql"):
-    continue
+    if not table.endswith(".sql"):
+        continue
 
-  with open(f"database/tables/{table}") as file:
-    query = file.read()
-    connection.cursor().execute(query)
-    connection.commit()
-
-
+    with open(f"database/tables/{table}") as file:
+        query = file.read()
+        connection.cursor().execute(query)
+        connection.commit()
