c3755fbe9b10b4873ca06f7379059a8c8f859a4c
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-03-29 13:08:07 -0300

    first version

---
+++ src/db.js
@@ -0,0 +1,80 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mysql_credentials = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+};
+
+async function connect() {
+  if (global.connection && global.connection.state !== "disconnected")
+    return global.connection;
+
+  const mysql = require("mysql2/promise");
+  const url = require("url");
+  const url_from_env = `mysql://${mysql_credentials.user}:${mysql_credentials.password}@${mysql_credentials.host}:${mysql_credentials.port}/${mysql_credentials.database}`;
+  const connection = await mysql.createConnection(url_from_env);
+  console.log("Conectou no MySQL!");
+  global.connection = connection;
+  return connection;
+}
+
+async function selectAllFraternityUsers() {
+  const conn = await connect();
+  const [rows] = await conn.query(
+    "SELECT * FROM users WHERE ID IN (SELECT USER_ID FROM vw_allFraternityUsersActive)"
+    // "SELECT * FROM users WHERE ID IN (13299)"
+  );
+  return rows;
+}
+
+async function selectAllAdmUsers() {
+  const conn = await connect();
+  const [rows] = await conn.query(
+    "SELECT * FROM users WHERE ID IN (SELECT USER_ID FROM vw_allAdmUsers)"
+  );
+  return rows;
+}
+
+async function saveWhatsappId(userId, whatsappId) {
+  const conn = await connect();
+  const [rows] = await conn.query(
+    "UPDATE users SET WHATSAPP_ID = ? WHERE ID = ?",
+    [whatsappId, userId]
+  );
+  return rows;
+}
+
+async function createChannel(channelWhatsappId, channelName) {
+  const conn = await connect();
+  const [rows] = await conn.query(
+    "INSERT INTO channels (WHATSAPP_ID, NAME) VALUES (?, ?)",
+    [channelWhatsappId, channelName]
+  );
+  return rows;
+}
+
+async function getUser(userId) {
+  const conn = await connect();
+  const [rows] = await conn.query("SELECT * FROM users WHERE ID = ?", [userId]);
+  return rows;
+}
+
+async function getChannel(channelId) {
+  const conn = await connect();
+  const [rows] = await conn.query("SELECT * FROM channels WHERE ID = ?", [
+    channelId,
+  ]);
+  return rows;
+}
+module.exports = {
+  selectAllFraternityUsers,
+  selectAllAdmUsers,
+  saveWhatsappId,
+  createChannel,
+  getUser,
+  getChannel,
+};
---
+++ src/index.js
@@ -0,0 +1,133 @@
+const { Client, LocalAuth, GroupChat } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const db = require("./db");
+
+// create a function to format the phone number to 5541998119091 format
+function formatPhoneNumber(phoneNumberString) {
+  var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  var match = cleaned.match(/^(\d{2})(\d{2})(\d{4,5})(\d{4})$/);
+  if (match) {
+    return match[1] + match[2] + match[3] + match[4];
+  } else {
+    if (cleaned.length == 11) {
+      return "55" + cleaned;
+    }
+    if (cleaned.length == 10) {
+      return "55" + cleaned;
+    }
+  }
+}
+
+function saveWhatsappIdFromClient(client, user, formatted_phone) {
+  client
+    .getNumberId(formatted_phone)
+    .then((chat) => {
+      db.saveWhatsappId(user.ID, chat._serialized);
+    })
+    .catch((err) => {
+      console.log("Error getting chat for ", user.PHONE, formatted_phone);
+    });
+}
+
+function processUsersPhone(users, client) {
+  for (let user of users) {
+    if (user.PHONE) {
+      const formatted_phone = formatPhoneNumber(user.PHONE);
+      if (formatted_phone) {
+        saveWhatsappIdFromClient(client, user, formatted_phone);
+      }
+    } else {
+      console.log("User without phone", user.ID);
+    }
+  }
+}
+
+(async () => {
+  const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+      // args: ["--no-sandbox"],
+    },
+  });
+
+  client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
+
+  client.on("ready", () => {
+    console.log("Connected to WhatsApp!");
+
+    client.on("message_create", (message) => {
+      console.log(message);
+
+      if (message.body === "!ping") {
+        message.reply("pong");
+      }
+
+      if (message.body.startsWith("!sync")) {
+        const message_splitted = message.body.split(" ");
+        if (message_splitted.length == 0) {
+          const available_commands_array = [
+            "!sync whatsapp_id",
+            "!sync adm_whatsapp_id",
+            "!sync add_channel",
+            "!sync add_member",
+          ];
+          message.reply(
+            "Available commands:\n" + available_commands_array.join("\n")
+          );
+        } else {
+          const sync_code = message_splitted[1];
+          if (sync_code == "whatsapp_id") {
+            db.selectAllFraternityUsers().then((users) => {
+              processUsersPhone(users, client);
+            });
+          } else if (sync_code == "adm_whatsapp_id") {
+            db.selectAllAdmUsers().then((users) => {
+              processUsersPhone(users, client);
+            });
+          } else if (sync_code == "add_channel") {
+            const channel_id = message.to;
+            const channel_name = message_splitted.slice(2).join(" ");
+            db.createChannel(channel_id, channel_name);
+          } else if (sync_code == "add_member") {
+            console.log("Syncing add_member");
+            let user_id = message_splitted[2];
+            db.getUser(user_id).then((user) => {
+              user_id = user[0].WHATSAPP_ID;
+            });
+
+            let channel_id = 27;
+            db.getChannel(channel_id).then((channel) => {
+              channel_id = channel[0].WHATSAPP_ID;
+              console.log("Channel id", channel_id);
+              console.log("User id", user_id);
+              client.getChatById(channel_id).then((chat) => {
+                chat.addParticipants([user_id]);
+              });
+            });
+          } else if (sync_code == "add_member_all_fraternity") {
+            console.log("Syncing add_member_all_fraternity");
+            let channel_id = 27;
+            const whatsapp_ids = [];
+            db.getChannel(channel_id).then((channel) => {
+              channel_id = channel[0].WHATSAPP_ID;
+              console.log("Channel id", channel_id);
+            });
+            db.selectAllFraternityUsers().then((users) => {
+              for (let user of users) {
+                if (user.WHATSAPP_ID) {
+                  client.getChatById(channel_id).then((chat) => {
+                    chat.addParticipants([user.WHATSAPP_ID]);
+                  });
+                }
+              }
+            }
+          }
+        }
+        console.log("Message sync processed", message.body);
+      }
+    });
+  });
+  client.initialize();
+})();
